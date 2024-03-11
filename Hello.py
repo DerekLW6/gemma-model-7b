76935cd8511d58ddc78503a3b3c66b60b790844a
Derek <53124417+DerekLW6@users.noreply.github.com>
2024-03-11 03:49:12 +0000

    Switched to mixtral, much better

--- Hello.py
+++ Hello.py
@@ -4,9 +4,11 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-def generate_text(prompt, max_length=6000, num_return_sequences=8, temperature=0.7):
+def generate_text(prompt, max_length=13000, num_return_sequences=15, temperature=0.7):
     # url = "https://api-inference.huggingface.co/models/google/gemma-7b"
-    url = "https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2"
+    # url = "https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2"
+    url =  "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2" # Updated URL
+
     headers = {"Authorization": f"Bearer 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"} 
     payload = {
         "inputs": prompt,
@@ -47,25 +49,3 @@ if __name__ == "__main__":
     run()
 
 
-## Old Code
-# st.markdown(
-    #     """
-    #     Streamlit is an open-source app framework built specifically for
-    #     Machine Learning and Data Science projects.
-    #     **👈 Select a demo from the sidebar** to see some examples
-    #     of what Streamlit can do!
-    #     ### Want to learn more?
-    #     - Check out [streamlit.io](https://streamlit.io)
-    #     - Jump into our [documentation](https://docs.streamlit.io)
-    #     - Ask a question in our [community
-    #       forums](https://discuss.streamlit.io)
-    #     ### See more complex demos
-    #     - Use a neural net to [analyze the Udacity Self-driving Car Image
-    #       Dataset](https://github.com/streamlit/demo-self-driving)
-    #     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    # """
-    # )
-
-        # st.write("# Welcome to Streamlit! 👋")
-
-    # st.sidebar.success("Select a demo above.")
